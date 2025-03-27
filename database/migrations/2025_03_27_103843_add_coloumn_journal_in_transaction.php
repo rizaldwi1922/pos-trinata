f3e33cc98ce5b1bf3d2079453e28db61c6008fc6
Rizal Dwi Rahmawiyanto <Rizal Dwi Rahmawiyanto@Samatorgroup.com>
2025-03-27 11:03:37 +0700

    update

---
+++ database/migrations/2025_03_27_103843_add_coloumn_journal_in_transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->unsignedBigInteger('journal_id')->nullable();
+            $table->foreign('journal_id')->references('id')->on('journal_entrys');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['journal_id']);
+            $table->dropColumn(['journal_id']);
+        });
+    }
+};
